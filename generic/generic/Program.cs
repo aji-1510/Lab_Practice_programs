666a91f3f2236d7ac1211291662398f66f63d659
aji-1510 <93470761+aji-1510@users.noreply.github.com>
2022-04-08 11:08:06 +0530

    Add files via upload

---
+++ generic/generic/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace generic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            generic_class<int> i = new generic_class<int>();
+            generic_class<string> s = new generic_class<string>();
+            Console.WriteLine("this is integer value output");
+            i.genericMethod(500);
+            Console.WriteLine("\nthis is srting value output");
+            s.genericMethod("ajith");
+            Console.ReadLine();
+        }  
+    }
+}
---
+++ generic/generic/generic_class.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace generic
+{
+    public class generic_class<t>
+    {
+        public void genericMethod( t num)
+        {
+            Console.Write("\t{0}", num);
+
+        }
+    }
+}
