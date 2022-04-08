ce2420c62d612087e61c4bccc94e6c3fc356132a
aji-1510 <93470761+aji-1510@users.noreply.github.com>
2022-04-08 10:24:53 +0530

    Add files via upload

---
+++ quadriatic_Equation/quadriatic_Equation/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace quadriatic_Equation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* C# program to find the root of a quadratic equation
+                   ax * 2 + bx + c = 0 */
+
+            // intializing variable a,b and c as integer
+            int a, b, c;
+            char ch;
+            //initializing variable d,x1 and x2 as double
+            double d, x1, x2;
+            do
+            {
+                //getting value of a,b and c from user
+                Console.Write("Enter the value of a : ");
+                a = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter the value of b : ");
+                b = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter the value of c : ");
+                c = Convert.ToInt32(Console.ReadLine());
+
+                //perform the equation bxb -4xaxc
+                d = b * b - 4 * a * c;
+
+                //condition for check the roots
+                if (d == 0)
+                {
+                    //if condition is true then it will perform this statement
+                    Console.Write("Both roots are equal\n");
+                    x1 = -b / (2.0 * a);
+                    x2 = x1;
+                    Console.Write("Root1 = {0}\n", x1);
+                    Console.Write("Root2 = {0}\n", x2);
+                }
+                else if (d > 0)
+                {
+                    //statement for printing both roots are real
+                    Console.Write("Both roots are real and diff-2\n");
+
+                    x1 = (-b + Math.Sqrt(d)) / (2 * a);
+                    x2 = (-b - Math.Sqrt(d)) / (2 * a);
+
+                    Console.Write("Root1= {0}\n", x1);
+                    Console.Write("Root2= {0}\n", x2);
+                }
+                else
+                {
+                    //print the roots are imaginary
+                    Console.Write("Roots are imaginary \n");
+                }
+
+                Console.ReadKey();
+                Console.WriteLine("Do you want run again Y/N");
+                ch = Convert.ToChar(Console.ReadLine());
+
+
+            } while ((ch == 'y') || (ch == 'Y'));
+        }
+    }
+}
