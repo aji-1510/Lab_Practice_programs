112ef9de6afe5c78c0e9a7587e7feda9ca7f4cc4
aji-1510 <93470761+aji-1510@users.noreply.github.com>
2022-04-08 11:46:55 +0530

    Add files via upload

---
+++ loop_121_12321/loop_121_12321/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace loop_121_12321
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n,i,j;
+            Console.WriteLine("loop program");
+            Console.WriteLine("enter the how many times run the pyramid shap\n");
+            n = Convert.ToInt32(Console.ReadLine());
+            for(i=1;i<=n;i++)
+            {
+                for (j = i; j <= n - 1; j++)
+                {
+                    Console.Write(" ");
+                }
+                for(j=1;j<=i;j++)
+                {
+                    Console.Write(j);  
+                }
+                for(int k=i-1;k>=1;k--)
+                {
+                    Console.Write(k);
+                }
+                Console.Write("\n ");
+            }
+
+        }
+    }
+}
