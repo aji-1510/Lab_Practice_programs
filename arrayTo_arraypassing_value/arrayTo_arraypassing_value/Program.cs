100b1b8f9ddb20ecee0672520622dcd5f557e7ad
aji-1510 <93470761+aji-1510@users.noreply.github.com>
2022-04-08 12:02:07 +0530

    Add files via upload

---
+++ arrayTo_arraypassing_value/arrayTo_arraypassing_value/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace arrayTo_arraypassing_value
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] name1 = new string[] { "AJiht", "sangee", "siva" };
+            string[] name2 = new string[3];
+            Console.WriteLine("frist array elements ");
+            foreach(string nam in name1)
+            {
+                Console.WriteLine(nam);
+            }
+            for(int i=0;i<name1.Length;i++)
+            {
+                name2[i] = name1[i];
+            }
+            Console.WriteLine("\n\nsecond array elements ");
+            foreach(string nam2 in name2)
+            {
+                Console.WriteLine(nam2);
+            }
+
+
+        }
+    }
+}
