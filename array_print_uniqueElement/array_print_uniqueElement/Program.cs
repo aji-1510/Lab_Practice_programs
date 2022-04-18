77ab4858bee1dee2dd74d4389c253ac8994ba227
aji-1510 <93470761+aji-1510@users.noreply.github.com>
2022-04-18 12:24:34 +0530

    Add files via upload

---
+++ array_print_uniqueElement/array_print_uniqueElement/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace array_print_uniqueElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Write a C# program to input elements in array
+              and print all unique elements in array. */
+
+            int[] arr = new int[50];
+
+            int i, j, k, n, c = 0;
+
+            Console.Write("Input the number of elements : ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the elements");
+            for (i = 0; i < n; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.Write("Elements are : ");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write(" " + arr[i]);
+            }
+
+            Console.Write("\nThe unique elements are : ");
+            for (i = 0; i < n; i++)
+            {
+                c = 0;
+
+                for (j = 0; j < i - 1; j++)
+                {
+
+                    if (arr[i] == arr[j])
+                    {
+                        c++;
+                    }
+                }
+
+                for (k = i + 1; k < n; k++)
+                {
+
+                    if (arr[i] == arr[k])
+                    {
+                        c++;
+                    }
+
+                    if (arr[i] == arr[i + 1])
+                    {
+                        i++;
+                    }
+                }
+
+                if (c == 0)
+                {
+                    Console.Write("{0} ", arr[i]);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
