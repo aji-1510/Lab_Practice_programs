2e4d73fe3a1a8ed8fd684e53f3f566c5da3c7bf6
aji-1510 <93470761+aji-1510@users.noreply.github.com>
2022-04-18 12:58:53 +0530

    Add files via upload

---
+++ max_min_arr/max_min_arr/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace max_min_arr
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Write a C# program to input elements in an array from user,
+               find maximum and minimum element in array. */
+
+            Console.WriteLine("Enter the elements : ");
+            int[] arr = new int[5];
+            int i;
+            for (i = 0; i < 5; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+
+            }
+
+            Console.Write("Elements are : ");
+            for (i = 0; i < 5; i++)
+            {
+                Console.Write(" " + arr[i]);
+
+            }
+            Console.Write("\n");
+
+            int max, min;
+            max = arr[0];
+            min = arr[0];
+            for (i = 0; i < 5; i++)
+            {
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                }
+            }
+            Console.WriteLine("Maximum element is {0}", max);
+            Console.WriteLine("Minimum element is {0}", min);
+
+            Console.ReadKey();
+        }
+    }
+}
